344df5862445213555fa51a6adc248880a56e55b
naveen <enreddy619@gmail.com>
2024-02-03 17:39:17 +0800

    fgdgfd

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAVRUVQASYVH4MLZ7J"
-  secret_key = "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"
+  access_key = "AKIAVRUVQASYSYLZKSNO"
+  secret_key = "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"
 }
 
 resource "aws_instance" "myec2" {
