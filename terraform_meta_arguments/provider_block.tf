6c265b2cd4ffc4bc39591050578ef113c7f3c68a
naveen <enreddy619@gmail.com>
2024-02-07 15:19:55 +0800

    for_each is added.

---
+++ terraform_meta_arguments/provider_block.tf
@@ -0,0 +1,6 @@
+provider "aws" { // 'aw' is a local name, it should match with local name of require_providers in terraform block.
+  region  = "ap-south-1"
+  profile = "terraform"
+  #   access_key = "AKIAVRUVQASYSYLZKSNO"
+  #   secret_key = "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"
+}
