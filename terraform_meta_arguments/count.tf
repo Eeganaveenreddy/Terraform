9d76618e1e5ab383cc781e95b713fe20d7e7b27a
naveen <enreddy619@gmail.com>
2024-02-07 15:05:16 +0800

    count file is added

--- terraform_meta_arguments/count.tf
+++ terraform_meta_arguments/count.tf
@@ -2,14 +2,14 @@ terraform {
   required_version = "~> 1.6"
 
   required_providers {
-    aw = { ## here 'aw' is local name, it should match with provider local name
+    aws = { ## here 'aws' is local name, it should match with provider local name
       version = ">= 5"
       source  = "hashicorp/aws"
     }
   }
 }
 
-provider "aw" { // 'aw' is a local name, it should match with local name of require_providers in terraform block.
+provider "aws" { // 'aws' is a local name, it should match with local name of require_providers in terraform block.
   region  = "ap-south-1"
   profile = "terraform"
   #   access_key = "AKIAVRUVQASYSYLZKSNO"
@@ -17,11 +17,11 @@ provider "aw" { // 'aw' is a local name, it should match with local name of requ
 }
 
 resource "aws_instance" "myec2" {
-    ami = "ami-03f4878755434977f"
-    instance_type = "t2.micro"
-    count = 5
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
+  count         = 5
 
-    tags = {
-        Name = web-${count.index}
-    }
+  tags = {
+    "Name" = "web-${count.index}"
+  }
 }
